47657935435be0f6c64e886cc8152adf13b23c3c
Ritikve03 <ritik.rv03@gmail.com>
2023-02-27 10:00:47 +0530

    asd

--- sneaker-head/src/components/Home/Category/Category.jsx
+++ sneaker-head/src/components/Home/Category/Category.jsx
@@ -1,22 +1,26 @@
-import { useParams } from "react-router-dom";
-import useFetch from "../../hooks/useFetch";
-import Products from "../Products/Products";
+import { useNavigate } from "react-router-dom";
 import "./Category.scss";
-const Category = () => {
-    const { id } = useParams();
-    const { data } = useFetch(
-        `/api/products?populate=*&[filters][categories][id]=${id}`
-    );
+
+const Category = ({ categories }) => {
+    console.log(categories)
+    const navigate = useNavigate();
     return (
-        <div className="category-main-content">
-            <div className="layout">
-                <div className="category-title">
-                    {
-                        data?.data?.[0]?.attributes?.categories?.data?.[0]
-                            ?.attributes?.title
-                    }
-                </div>
-                <Products innerPage={true} products={data} />
+        <div className="shop-by-category">
+            <div className="categories">
+                {categories?.data?.map((item) => (
+                    <div
+                        key={item.id}
+                        className="category"
+                        onClick={() => navigate(`/category/${item.id}`)}
+                    >
+                        <img
+                            src={
+                                process.env.REACT_APP_STRIPE_APP_DEV_URL +
+                                item.attributes.img.data.attributes.url
+                            }
+                        />
+                    </div>
+                ))}
             </div>
         </div>
     );
